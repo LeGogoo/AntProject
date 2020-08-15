27517c252359605c551f00aa47b34fe2b705ff04
david <903834068@qq.com>
2020-08-15 17:12:45 +0800

    add project

---
+++ AntProject/Assets/Scenes/CameraManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    public Transform Ant;
+
+    private void Update()
+    {
+        transform.position = new Vector3(Ant.position.x, transform.position.y, Ant.position.z);
+    }
+}
